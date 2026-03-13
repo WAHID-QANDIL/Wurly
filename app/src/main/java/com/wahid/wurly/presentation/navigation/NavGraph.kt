cea8a823cf981fbda425553164f6679c048e1d18
wahid <wahidqandill@gmail.com>
2026-03-13 12:04:31 +0200

    Add navigation graph for app screens with transitions

---
+++ app/src/main/java/com/wahid/wurly/presentation/navigation/NavGraph.kt
@@ -0,0 +1,49 @@
+package com.wahid.wurly.presentation.navigation
+
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.wahid.wurly.presentation.screen.home.home
+import com.wahid.wurly.presentation.screen.forecast.forecast
+import com.wahid.wurly.presentation.screen.settings.settings
+import com.wahid.wurly.presentation.screen.favorites.favorites
+import com.wahid.wurly.presentation.screen.addfavorite.addFavorite
+import com.wahid.wurly.presentation.screen.alerts.alerts
+import com.wahid.wurly.presentation.screen.map.map
+
+
+private const val TRANSITION_DURATION = 200
+
+@Composable
+fun NavGraph(
+    navHostController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navHostController,
+        startDestination = DestinationRoutes.Home,
+        enterTransition = {
+            fadeIn(
+                animationSpec = tween(TRANSITION_DURATION)
+            )
+        }, exitTransition = {
+            fadeOut(
+                animationSpec = tween(TRANSITION_DURATION)
+            )
+        }
+    ) {
+        home()
+        forecast()
+        settings()
+        favorites()
+        addFavorite()
+        map()
+        alerts()
+    }
+
+}
