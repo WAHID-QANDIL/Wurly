742224ab8d99ac87dee3d94172af0febc30ad361
wahid <wahidqandill@gmail.com>
2026-03-13 12:06:15 +0200

    Add KSP, Kotlin Serialization, and Dagger Hilt plugins; update dependencies for Room, Retrofit, and Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,17 @@
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.android)
-}
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.dagger.hilt)
 
+}
 android {
     namespace = "com.wahid.wurly"
     compileSdk = 36
+    ndkVersion = "27.0.12077973"
     defaultConfig {
         applicationId = "com.wahid.wurly"
         minSdk = 28
@@ -15,8 +20,20 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+        @Suppress("UnstableApiUsage")
+        externalNativeBuild {
+            cmake {
+                cppFlags("-std=c++17")
+            }
+        }
 
+    }
+    externalNativeBuild {
+        cmake {
+            path("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -32,7 +49,9 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
+
 
 }
 
@@ -46,12 +65,54 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
+    //Retrofit
+    implementation(libs.kotlinx.serialization.retrofit.converter)
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    //Serialization
+    implementation(libs.kotlinx.serialization.json)
+
+    //Lifecycle coroutines support
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    //Room
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+
+
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
+
+
+
+    //Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+//    implementation(libs.androidx.hilt.work)
+    ksp(libs.hilt.compiler)
+
+    //Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
+
+    //Testing
+
+
+
+    // Junit
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    //Espresso
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //Room
+    testImplementation(libs.androidx.room.testing)
+
+    androidTestImplementation(platform(libs.androidx.compose.bom))
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation(libs.cloudy)
 }
