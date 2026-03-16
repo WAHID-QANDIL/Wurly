67b9c9cae7888dc5ea4e272cbb8427d9c51f7e59
wahid <wahidqandill@gmail.com>
2026-03-16 17:14:17 +0200

    Add LocaleHelper utility; implement method to apply app locale based on language code

---
+++ app/src/main/java/com/wahid/wurly/utils/LocaleHelper.kt
@@ -0,0 +1,16 @@
+package com.wahid.wurly.utils
+
+import android.content.Context
+import android.content.res.Configuration
+import java.util.Locale
+
+object LocaleHelper {
+    fun applyAppLocale(context: Context, languageCode: String) {
+        val locale = Locale(languageCode.lowercase(Locale.ROOT))
+        Locale.setDefault(locale)
+        val resources = context.applicationContext.resources
+        val config = Configuration(resources.configuration)
+        config.setLocale(locale)
+        resources.updateConfiguration(config, resources.displayMetrics)
+    }
+}
