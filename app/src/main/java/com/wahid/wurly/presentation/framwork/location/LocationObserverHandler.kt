850ce39d1f71ec74dc29e2415bc2810784396940
wahid <wahidqandill@gmail.com>
2026-03-16 04:08:42 +0200

    Add location services implementation; include permissions and observer for location updates

---
+++ app/src/main/java/com/wahid/wurly/presentation/framwork/location/LocationObserverHandler.kt
@@ -0,0 +1,28 @@
+package com.wahid.wurly.presentation.framwork.location
+
+import android.util.Log
+import com.mapbox.common.location.Location
+import com.mapbox.common.location.LocationObserver
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+class LocationObserverHandler(
+    val onReceivedLocation:suspend (locations: List<Location?>) -> Unit,
+    private val coroutineScope: CoroutineScope
+) : LocationObserver {
+    companion object {
+        private const val TAG = "LocationObserver"
+    }
+
+    override fun onLocationUpdateReceived(locations: List<Location?>) {
+        Log.d(
+            TAG,
+            "onLocationUpdateReceived: Received location update: ${locations.joinToString(", ")}"
+        )
+
+        coroutineScope.launch {
+            onReceivedLocation(locations)
+        }
+
+    }
+}
